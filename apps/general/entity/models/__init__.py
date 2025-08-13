0108c3981e3558a939c74f6cecad5a75915e835e
Reyving Ramirez <medinafabriany@gmail.com>
2025-08-13 07:12:05 -0500

    app generel en swagger

--- apps/general/entity/models/__init__.py
+++ apps/general/entity/models/__init__.py
@@ -1,2 +1,11 @@
-from .person import Person
-__all__=['Person']
+from .Aprendiz import Aprendiz
+from .Center import Center
+from .Ficha import Ficha
+from .Instructor import Instructor
+from .Program import Program
+from .Regional import Regional
+from .Sede import Sede
+
+__all__ = [
+    'Aprendiz', 'Center', 'Ficha', 'Instructor', 'Program', 'Regional', 'Sede'
+]
--- apps/general/urls.py
+++ apps/general/urls.py
@@ -1,3 +1,23 @@
-from django.urls import path
-urlpatterns=[
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.general.views.AprendizViewset import AprendizViewSet
+from apps.general.views.CenterViewset import CenterViewSet
+from apps.general.views.FichaViewset import FichaViewSet
+from apps.general.views.InstructorViewset import InstructorViewSet
+from apps.general.views.ProgramViewset import ProgramViewSet
+from apps.general.views.RegionalViewset import RegionalViewSet
+from apps.general.views.SedeViewset import SedeViewSet
+
+router = DefaultRouter()
+router.register(r'aprendices', AprendizViewSet, basename='aprendices')
+router.register(r'centers', CenterViewSet, basename='centers')
+router.register(r'fichas', FichaViewSet, basename='fichas')
+router.register(r'instructors', InstructorViewSet, basename='instructors')
+router.register(r'programs', ProgramViewSet, basename='programs')
+router.register(r'regionals', RegionalViewSet, basename='regionals')
+router.register(r'sedes', SedeViewSet, basename='sedes')
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
