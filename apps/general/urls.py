e346128752b93b1e43ecb3252bbf53c09a4ab44b
Reyving Ramirez <medinafabriany@gmail.com>
2025-08-13 21:28:09 -0500

    estructuracion de swagger

--- apps/general/urls.py
+++ apps/general/urls.py
@@ -1,22 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from apps.general.views.AprendizViewset import AprendizViewSet
-from apps.general.views.CenterViewset import CenterViewSet
-from apps.general.views.FichaViewset import FichaViewSet
-from apps.general.views.InstructorViewset import InstructorViewSet
-from apps.general.views.ProgramViewset import ProgramViewSet
-from apps.general.views.RegionalViewset import RegionalViewSet
-from apps.general.views.SedeViewset import SedeViewSet
+from apps.general.views.AprendizViewset import AprendizViewset
+from apps.general.views.CenterViewset import CenterViewset
+from apps.general.views.FichaViewset import FichaViewset
+from apps.general.views.InstructorViewset import InstructorViewset
+from apps.general.views.ProgramViewset import ProgramViewset
+from apps.general.views.RegionalViewset import RegionalViewset
+from apps.general.views.SedeViewset import SedeViewset
 
 router = DefaultRouter()
-router.register(r'aprendices', AprendizViewSet, basename='aprendices')
-router.register(r'centers', CenterViewSet, basename='centers')
-router.register(r'fichas', FichaViewSet, basename='fichas')
-router.register(r'instructors', InstructorViewSet, basename='instructors')
-router.register(r'programs', ProgramViewSet, basename='programs')
-router.register(r'regionals', RegionalViewSet, basename='regionals')
-router.register(r'sedes', SedeViewSet, basename='sedes')
+router.register(r'aprendices', AprendizViewset, basename='general_aprendices')
+router.register(r'centers', CenterViewset, basename='general_centers')
+router.register(r'fichas', FichaViewset, basename='general_fichas')
+router.register(r'instructors', InstructorViewset, basename='general_instructors')
+router.register(r'programs', ProgramViewset, basename='general_programs')
+router.register(r'regionals', RegionalViewset, basename='general_regionals')
+router.register(r'sedes', SedeViewset, basename='general_sedes')
 
 urlpatterns = [
     path('', include(router.urls)),
