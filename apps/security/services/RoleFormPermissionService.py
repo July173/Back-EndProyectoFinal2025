c11048a2a12a8eef9d4882723a2b9d3c8951c2a0
Reyving <medinafabriany@gmail.com>
2025-09-09 14:37:19 -0500

    menu

--- apps/security/services/RoleFormPermissionService.py
+++ apps/security/services/RoleFormPermissionService.py
@@ -1,7 +1,20 @@
+from apps.security.entity.serializers.MenuSerializer import MenuSerializer
 from core.base.services.implements.baseService.BaseService import BaseService
 from apps.security.repositories.RoleFormPermissionRepository import RolFormPermissionRepository
 
 class RolFormPermissionService(BaseService):
+
+    def get_menu(self, user_id: int):
+        # Llama al repository para obtener la data cruda
+        menu_data = self.repository.get_menu(user_id)
+
+        # Validación: si no hay menú
+        if not menu_data:
+            return None
+
+        # Serializa la lista de menús con sus formularios
+        serializer = MenuSerializer(menu_data, many=True)
+        return serializer.data
 
     def update_role_with_permissions(self, pk, data):
         """
