10a65892d61bca67a935452319629e1a181f0855
Reyving <medinafabriany@gmail.com>
2025-09-09 14:49:19 -0500

    ..

--- apps/security/services/RoleFormPermissionService.py
+++ apps/security/services/RoleFormPermissionService.py
@@ -1,20 +1,7 @@
-from apps.security.entity.serializers.MenuSerializer import MenuSerializer
 from core.base.services.implements.baseService.BaseService import BaseService
 from apps.security.repositories.RoleFormPermissionRepository import RolFormPermissionRepository
-from apps.security.entity.serializers.RolFormPermission.MenuSerializer import MenuSerializer
+
 class RolFormPermissionService(BaseService):
-
-    def get_menu(self, user_id: int):
-        # Llama al repository para obtener la data cruda
-        menu_data = self.repository.get_menu(user_id)
-
-        # Validación: si no hay menú
-        if not menu_data:
-            return None
-
-        # Serializa la lista de menús con sus formularios
-        serializer = MenuSerializer(menu_data, many=True)
-        return serializer.data
 
     def update_role_with_permissions(self, pk, data):
         """
@@ -51,7 +38,6 @@ class RolFormPermissionService(BaseService):
             'updated_permissions': total_created
         }
 
-
     def create_role_with_permissions(self, data):
         """
         Crea un nuevo rol y asigna permisos a uno o varios formularios.
@@ -86,17 +72,3 @@ class RolFormPermissionService(BaseService):
         }
     def __init__(self):
         self.repository = RolFormPermissionRepository()
-
-    def get_menu(self, user_id: int):
-        # Llama al repository para obtener la data cruda
-        menu_data = self.repository.get_menu(user_id)
-
-        # Validación: si no hay menú
-        if not menu_data:
-            return None
-
-        # Serializa la lista de menús con sus formularios
-        serializer = MenuSerializer(menu_data, many=True)
-        return serializer.data
-
-
