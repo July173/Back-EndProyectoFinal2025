2685a2ff0bab432cf0b59c78edeee46b18546c18
Reyving <medinafabriany@gmail.com>
2025-09-17 14:44:16 -0500

    enum usado

--- apps/general/entity/serializers/CreateInstructor/CreateInstructorSerializer.py
+++ apps/general/entity/serializers/CreateInstructor/CreateInstructorSerializer.py
@@ -1,4 +1,6 @@
+
 from rest_framework import serializers
+from apps.security.entity.enums.document_type_enum import DocumentType
 
 
 class CreateInstructorSerializer(serializers.Serializer):
@@ -8,7 +10,7 @@ class CreateInstructorSerializer(serializers.Serializer):
     first_last_name = serializers.CharField()
     second_last_name = serializers.CharField(required=False, allow_blank=True)
     phone_number = serializers.IntegerField(required=False)
-    type_identification = serializers.CharField()
+    type_identification = serializers.ChoiceField(choices=[dt.value for dt in DocumentType])
     number_identification = serializers.IntegerField()
     # Campos de User
     email = serializers.EmailField()
