58ae28ccc6744839591082e066a274453d37abd6
Reyving <medinafabriany@gmail.com>
2025-10-01 19:26:46 -0500

    tabla recursiva cambiada de relacion

--- apps/general/entity/models/LegalSection.py
+++ apps/general/entity/models/LegalSection.py
@@ -3,7 +3,7 @@ from .LegalDocument import LegalDocument
 
 class LegalSection(models.Model):
     documentId = models.ForeignKey(LegalDocument, on_delete=models.CASCADE, related_name='sections')
-    parentId = models.OneToOneField('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='child')
+    parentId = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='children')
     order = models.IntegerField()
     code = models.CharField(max_length=20)
     title = models.CharField(max_length=200)
