9c44b777a344ea0194196cdbeff680c3a90efe4e
july <danielitha345ra@gmail.com>
2025-10-11 08:32:49 -0500

    Ajuste en el put de user

--- apps/security/entity/serializers/User/UserSerializer.py
+++ apps/security/entity/serializers/User/UserSerializer.py
@@ -17,18 +17,41 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_aprendiz(self, obj):
         # Buscar si la persona está vinculada como Aprendiz
-        from apps.general.entity.models import Aprendiz
-        aprendiz = Aprendiz.objects.filter(person=obj.person).first()
+        from apps.general.entity.models import Aprendiz, Ficha, Program
+        aprendiz = Aprendiz.objects.filter(person=obj.person).select_related('ficha__program').first()
         if aprendiz:
-            return AprendizSerializer(aprendiz).data
+            data = AprendizSerializer(aprendiz).data
+            ficha = aprendiz.ficha
+            if ficha and ficha.program:
+                data['programa'] = {
+                    'id': ficha.program.id,
+                    'name': ficha.program.name
+                }
+            else:
+                data['programa'] = None
+            return data
         return None
 
     def get_instructor(self, obj):
         # Buscar si la persona está vinculada como Instructor
-        from apps.general.entity.models import Instructor
+        from apps.general.entity.models import Instructor, PersonSede, Sede, Center, Regional
         instructor = Instructor.objects.filter(person=obj.person).first()
         if instructor:
-            return InstructorSerializer(instructor).data
+            data = InstructorSerializer(instructor).data
+            # Buscar la sede vinculada
+            person_sede = PersonSede.objects.filter(PersonId=instructor.person).select_related('SedeId__center__regional').first()
+            if person_sede and person_sede.SedeId:
+                sede = person_sede.SedeId
+                centro = sede.center if hasattr(sede, 'center') else None
+                regional = centro.regional if centro and hasattr(centro, 'regional') else None
+                data['sede'] = {'id': sede.id, 'name': sede.name} if sede else None
+                data['centro'] = {'id': centro.id, 'name': centro.name} if centro else None
+                data['regional'] = {'id': regional.id, 'name': regional.name} if regional else None
+            else:
+                data['sede'] = None
+                data['centro'] = None
+                data['regional'] = None
+            return data
         return None
 
     class Meta:
